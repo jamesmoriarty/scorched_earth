77c7ceeb6e7387b1d68a27679b6395272784ced3
James Moriarty <jamespaulmoriarty@gmail.com>
2013-03-30 17:11:55 +1100

    feature: new game font.

--- lib/states/game_over.rb
+++ lib/states/game_over.rb
@@ -1,24 +1,24 @@
 class GameOver < Chingu::GameState
   def setup
     self.input = { :esc => :exit, :return => :try_again }
-    @layover = Gosu::Color.new(0x99000000)
+    @layover = Gosu::Color.new(0x22000000)
   end
 
   def draw
     super
-    # previous_game_state.draw
-    # fill(@layover)
+    previous_game_state.draw
+    fill(@layover)
     draw_text
   end
 
   def draw_text
-    @font     = Gosu::Font.new($window, "verdana", 30)
-
-    text      = "GAME OVER (ESC to quit, RETURN to try again!)"
-    x         = ($window.width/2) - (@font.text_width(text)/2)
-    y         = $window.height/2
-    z         = 0
     factor_x, factor_y = 1, 1
+
+    @font     = Gosu::Font["minercraftory.ttf", 48] #.new($window, "verdana", 12)
+    text      = "GAME OVER (ESC to quit, RETURN to try again!)"
+    x         = ($window.width/2) - (@font.text_width(text, factor_x)/2)
+    y         = $window.height/8
+    z         = 0
     color     = Gosu::Color::WHITE
 
     @font.draw(text, x, y, 500, factor_x, factor_y, color)
