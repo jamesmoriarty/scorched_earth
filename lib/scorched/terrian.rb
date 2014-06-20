eef42fc3e32e7c9b05ae080ae2324a6476ceb0a0
James Moriarty <jamespaulmoriarty@gmail.com>
2014-06-20 23:15:16 +1000

    refinement: simplify #deform.

--- lib/scorched/terrian.rb
+++ lib/scorched/terrian.rb
@@ -8,11 +8,9 @@ module Scorched
 
     def deform(shot)
       x1 = shot.x - shot.radius
-      x1 = [0, x1].max.to_i
       x2 = shot.x + shot.radius
-      x2 = [size, x2].min.to_i
 
-      Range.new(x1, x2).to_a.each do |x|
+      Range.new(x1.to_i, x2.to_i).to_a.each do |x|
         cycle    = (x - shot.x).to_f / (shot.radius * 2).to_f + 0.5
         delta    = Math.sin(Math::PI * cycle) * shot.radius
         self[x] -= delta.to_i if x >= 0 && x < size
