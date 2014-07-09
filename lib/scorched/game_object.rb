9ffb4cc4a84ae8b04976d47db4b114c7f34d7f21
James Moriarty <jamespaulmoriarty@gmail.com>
2014-07-09 20:13:29 +1000

    bugfix: modules.

--- lib/scorched/game_object.rb
+++ lib/scorched/game_object.rb
@@ -1,32 +1,34 @@
-class GameObject
-  def update
-  end
-
-  def render(win, height)
-  end
-
-  def destroy
-    self.class.all.delete(self)
-  end
-
-  class << self
-    def all
-      @collection ||= []
+module Scorched
+  class GameObject
+    def update
     end
 
-    def get(index = 0)
-      @collection[index]
+    def render(win, height)
     end
 
-    def create(*args)
-      all << new(*args)
-      all.last
+    def destroy
+      self.class.all.delete(self)
     end
 
-    def descendants
-      @cache ||= begin
-        ObjectSpace.each_object(Class).select do |klass|
-          klass < self
+    class << self
+      def all
+        @collection ||= []
+      end
+
+      def get(index = 0)
+        @collection[index]
+      end
+
+      def create(*args)
+        all << new(*args)
+        all.last
+      end
+
+      def descendants
+        @cache ||= begin
+          ObjectSpace.each_object(Class).select do |klass|
+            klass < self
+          end
         end
       end
     end
