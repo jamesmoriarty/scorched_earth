60a2dc8fb149712413dcf1d225d49fd704b343fa
James Moriarty <james.moriarty@rea-group.com>
2016-09-14 15:25:23 +1000

    shot cleanup.

--- lib/scorched/shot.rb
+++ lib/scorched/shot.rb
@@ -3,8 +3,12 @@ require "scorched/entity"
 module Scorched
   class Shot
     include Entity
+    include Helpers
 
-    GRAVITY = 2000
+    GRAVITY     = 2000
+    LINE_LENGTH = 10
+    LINE_WIDTH  = 2
+    LINE_COLOR  = Ray::Color.black
 
     def update(delta)
       @velocity_y -= GRAVITY * delta
@@ -14,8 +18,11 @@ module Scorched
 
     def draw(win)
       width, height = *win.size
+      degrees       = angle(velocity_y, velocity_x)
+      x1, y1        = x, height - y
+      x2, y2        = x1 + offset_x(degrees, LINE_LENGTH), y1 + offset_y(degrees, LINE_LENGTH)
 
-      win.draw Ray::Polygon.circle([x, height - y], 5, Ray::Color.black)
+      win.draw Ray::Polygon.line([x1, y1], [x2, y2], LINE_WIDTH, LINE_COLOR)
     end
   end
 end
